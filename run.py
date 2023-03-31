66afd7877d1f0f46e631a47ebe9ffac51e712318
kendrick010 <kendrick@Kendricks-MacBook-Air.local>
2023-03-30 20:48:05 -0700

    fix templating and static paths

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
  
 @app.route('/')
 def home():
